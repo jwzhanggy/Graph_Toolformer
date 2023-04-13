e6e4d5d8bdc1eccfad156db068f7473a72374075
Jiawei Zhang <jwzhanggy@gmail.com>
2023-04-13 00:37:05 -0700

    Add files via upload

--- code/Module_Query_Parser.py
+++ code/Module_Query_Parser.py
@@ -1,4 +1,6 @@
 import re
+import time
+
 
 class Module_Query_Parser():
 
@@ -7,29 +9,51 @@ class Module_Query_Parser():
         self.description = description
 
     def parse_function_calls(self, input_string):
-        function_call_pattern = r'\b([a-zA-Z_][a-zA-Z0-9_]*)\s*\((.*)\)'
+        function_call_pattern = r'\b([a-zA-Z_][a-zA-Z0-9_]*)\s*\(([^\]]*)\)'
         function_calls = re.findall(function_call_pattern, input_string)
 
         parsed_function_calls = []
-
         for function_call in function_calls:
             function_name, parameters_str = function_call
             parameter_list = []
+            nested_count = 0
+            nested_function_call_str = ''
             for parameter in parameters_str.split(','):
-                if '(' in parameter and ')' in parameter:
-                    parameter_list.append(self.parse_function_calls(parameter))
+                parameter = parameter.strip()
+                if '(' in parameter or ')' in parameter:
+                    count_left_parentheses = parameter.count('(')
+                    count_right_parentheses = parameter.count(')')
+                    nested_count = nested_count + count_left_parentheses - count_right_parentheses
+                    nested_function_call_str += ','+parameter
+                    if nested_count == 0:
+                        parameter_list.append(self.parse_function_calls(nested_function_call_str))
+                        nested_function_call_str = ''
                 else:
-                    parameter_list.append(parameter)
+                    if nested_count == 0:
+                        parameter_list.append(parameter)
+                    else:
+                        nested_function_call_str += ','+parameter
             parsed_function_calls.append((function_name, parameter_list))
 
         return parsed_function_calls
 
     def parse_output_variable(self, input_string):
-        output_variable_pattern = r'-->([a-zA-Z_][a-zA-Z0-9_]*)'
-        output_variable = re.search(output_variable_pattern, input_string)
-        return output_variable.group(1) if output_variable else None
+        output_variable_pattern = r'\)\s*([-->]*)([a-zA-Z0-9_\s]*)\s*\]'
+        output_variables = re.findall(output_variable_pattern, input_string)
+        output_variable_list = []
+        for output_variable in output_variables:
+            if '-->' in output_variable:
+                output_variable_list.append(True)
+            else:
+                output_variable_list.append(False)
+        return output_variable_list
 
-    def parse_query(self, query_text_input):
-        function_parameters = self.parse_function_calls(query_text_input)
-        output_variable = self.parse_output_variable(query_text_input)
+    def parse_query(self, query_input):
+        function_parameters = self.parse_function_calls(query_input)
+        output_variable = self.parse_output_variable(query_input)
         return function_parameters, output_variable
+
+if __name__ == "__main__":
+    input = 'Output: Nodes [ GR ( GL ( "dodecahedral_graph", GB("a", GS("c"))), "periphery" )  -->  r ] have the largest eccentricity [GR(GL("dodecahedral_graph"), "eccentricity")] in the dodecahedral graph, which make them part of its periphery.'
+    parser = Module_Query_Parser()
+    print(parser.parse_query(query_input=input))
